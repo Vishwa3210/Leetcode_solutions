b20c551ab5ad632ad35d77d60187ff8a8dcda4df
Vishwa3210 <148730645+Vishwa3210@users.noreply.github.com>
2026-04-27 17:50:50 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
+++ 1070-product-sales-analysis-iii/1070-product-sales-analysis-iii.sql
@@ -1,6 +1,6 @@
 select product_id, first_year as first_year, quantity, price
 from
-(select product_id, year as first_year, quantity, price, dense_rank() over
+(select product_id, year as first_year, quantity, price, rank() over
 (partition by product_id order by year ) as rnk 
 from Sales
 )t
